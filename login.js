5bdfc6fc075877cda266be6b383b036c647d8e60
SwampVoid <haydenjtate@gmail.com>
2026-08-18 09:36:17 +1000

    Quality of life

--- login.js
+++ login.js
@@ -3,13 +3,32 @@ var emailInput = document.getElementById("emailInput")
 var passWord = document.getElementById("passWordInput")
 var warnText = document.getElementById("warnText")
 
-loginBtn.addEventListener("click", function() {
-
+function checkDetails() {
     if (emailInput.value == "Admin" && passWord.value == "Admin") {
         window.location.href = "index.html"
     } else {
         console.log("Incorrect password or username")
 
         warnText.style.opacity = 1
+    }
+}
+
+loginBtn.addEventListener("click", function() {
+    checkDetails();
+})
+
+emailInput.addEventListener("keydown", function(event)  { 
+    if (event.key == "Enter") {
+        event.preventDefault();
+
+        checkDetails();
+    }
+})
+
+passWord.addEventListener("keydown", function(event)  { 
+    if (event.key == "Enter") {
+        event.preventDefault();
+
+        checkDetails();
     }
 })
